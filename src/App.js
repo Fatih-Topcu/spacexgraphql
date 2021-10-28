58ab55195b6197d306e243f13359d08438f106c0
Fatih-Topcu <fatihtopcu97@gmail.com>
2021-10-29 01:50:32 +0300

    Added some styling and graphql.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import LaunchPage from "./Pages/LaunchPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 
 function App() {
+
+  
   return (
-    <div>
+    <div style={{ margin: "3rem 5rem 3rem 5rem" }}>
       <HashRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/launch/:id" component={LaunchPage} />
           <Route exact path="/404" component={NotFoundPage} />
-              <Redirect to="/404" />
+          <Redirect to="/404" />
         </Switch>
       </HashRouter>
     </div>
--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import "../styles/styles.css";
 
 const NotFoundPage = () => {
-  return <div> Not Found 404</div>;
+  return (
+    <>
+      <div className="notfound-header page-header">Not Found - 404</div>
+      <Link to="/">
+        <div className="home-return-link">Click to return homepage</div>
+      </Link>
+    </>
+  );
 };
 
 export default NotFoundPage;

dce32a839ce7067a31dd8a52ad9630071d76a2f8
Fatih-Topcu <fatihtopcu97@gmail.com>
2021-10-29 03:31:13 +0300

    requirements are done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Gallery from "react-grid-gallery";
 import "../styles/styles.css";
 import { useQuery, gql } from "@apollo/client";
+import { withRouter } from "react-router-dom";
 
 const LAUNCHES = gql`
   {
@@ -28,89 +29,7 @@ const LAUNCHES = gql`
   }
 `;
 
-const HomePage = () => {
-  const exampleArray = [
-    {
-      src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
-      thumbnail:
-        "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
-      thumbnailWidth: 271,
-      thumbnailHeight: 320,
-      tags: [{ value: "Nature", title: "Nature | Flowers" }],
-      caption: "Orange Macro (Tom Eversley - isorepublic.com)",
-    },
-    {
-      src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
-      thumbnail:
-        "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 190,
-      tags: [
-        { value: "Architecture", title: "Architecture | Outdoors" },
-        { value: "Industrial", title: "Industrial" },
-      ],
-      caption: "286H (gratisography.com)",
-    },
-    {
-      src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
-      thumbnail:
-        "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 148,
-      tags: [
-        { value: "People", title: "People" },
-        { value: "Architecture", title: "Architecture | Outdoors" },
-        { value: "Industrial", title: "Industrial" },
-      ],
-      caption: "315H (gratisography.com)",
-    },
-    {
-      src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
-      thumbnail:
-        "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 213,
-      caption: "201H (gratisography.com)",
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
-      thumbnailWidth: 248,
-      thumbnailHeight: 320,
-      caption: "Big Ben (Tom Eversley - isorepublic.com)",
-    },
-    {
-      src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
-      thumbnail:
-        "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 113,
-      tags: [
-        { value: "People", title: "People" },
-        { value: "Industrial", title: "Industrial" },
-      ],
-      caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
-    },
-    {
-      src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
-      thumbnail:
-        "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
-      thumbnailWidth: 313,
-      thumbnailHeight: 320,
-      caption: "Wood Glass (Tom Eversley - isorepublic.com)",
-    },
-    {
-      src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
-      thumbnail:
-        "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 213,
-      tags: [{ value: "Nature", title: "Nature | Flowers" }],
-      caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
-    },
-  ];
-
+const HomePage = (props) => {
   const { data, loading, error } = useQuery(LAUNCHES);
 
   if (loading) return "Loading..";
@@ -121,13 +40,13 @@ const HomePage = () => {
   data.launchesPast.forEach((element) => {
     if (element.links.flickr_images.length > 0) {
       imageData.push({
+        src: element.links.flickr_images[0],
         thumbnail: element.links.flickr_images[0],
         id: element.id,
       });
     }
   });
 
-  console.log(imageData);
   return (
     <>
       <div className="home-header page-header"> SPACEX LAUNCHES</div>
@@ -135,11 +54,11 @@ const HomePage = () => {
       <Gallery
         images={imageData}
         onClickThumbnail={(e) => {
-          console.log(imageData[e].id);
+          props.history.push(`/launch/${imageData[e].id}`);
         }}
       />
     </>
   );
 };
 
-export default HomePage;
+export default withRouter(HomePage);
--- src/Pages/LaunchPage.jsx
+++ src/Pages/LaunchPage.jsx
@@ -1,12 +1,82 @@
-import React from 'react';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
+import LeftLogo from "../media/LeftArrow.png";
+import "../styles/styles.css";
+import Gallery from "react-grid-gallery";
 
-const LaunchPage = () =>{
+const LaunchPage = (props) => {
+  let id = props.match.params.id;
 
-    return (
-        <div>
-           LAUNCH PAGE
-        </div>
-    )
-}
+  const LAUNCH = gql`
+    {
+      launch(id: ${id}) {
+        mission_name
+        links {
+          flickr_images
+          article_link
+          video_link
+          wikipedia
+        }
+        launch_date_utc
+        id
+        details
+        launch_year
+        launch_success
+        rocket {
+          rocket_name
+        }
+        launch_site {
+          site_name
+        }
+      }
+    }
+  `;
+
+  const { data, loading, error } = useQuery(LAUNCH);
+
+  if (loading) return "Loading..";
+  if (error) return <pre>Error</pre>;
+
+  const launchData = data.launch;
+
+  if (launchData == null && !loading) {
+    props.history.push(`/404`);
+  }
+
+  let images = [];
+
+  launchData.links.flickr_images.forEach((el) => {
+    images.push({
+      src: el,
+      thumbnail: el,
+    });
+  });
+
+  return (
+    <>
+      <div>
+        <Link to="/">
+          <img className="back-button" src={LeftLogo} alt="Go Back" />
+        </Link>
+        <span className="mission-name">{launchData.mission_name}</span>
+      </div>
+      <br />
+      <div className="mission-date">Date: {launchData.launch_date_utc}</div>
+      <br />
+      <a href={launchData.links.video_link}>Watch Here</a> -{" "}
+      <a href={launchData.links.wikipedia}>Wikipedia Link</a>
+      <br />
+      <br />
+      <div style={{ fontWeight: "bold" }}>LAUNCH DETAILS</div>
+      <div style={{ marginBottom: "1rem" }}>{launchData.details}</div>
+      <div style={{ fontWeight: "bold", marginBottom: "1rem" }}>
+        Rocket : {launchData.rocket.rocket_name} - Launch Site :{" "}
+        {launchData.launch_site.site_name}{" "}
+      </div>
+      <Gallery images={images} />
+    </>
+  );
+};
 
 export default LaunchPage;
